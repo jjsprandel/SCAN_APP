2441addf09d49a841720dd174786b5444cb51b02
deandrehendrix <deandrehendrix96@gmail.com>
2025-04-08 20:06:15 -0400

    updated navbar to be mobile friendly

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { useAuth } from "../context/AuthContext";
 import "./CustomNavbar.css";
 
@@ -7,49 +7,60 @@ function CustomNavbar() {
   const { currentUser } = useAuth();
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/" className="ms-3 nav-link-custom">
-        <img
-          src="/imgs/logo.png"
-          alt="Profile"
-          className="img-fluid"
-          style={{
-            width: "30px",
-            height: "30px",
-            borderRadius: "50%",
-            marginRight: "10px",
-          }}
-        />
-        SCAN
-      </Navbar.Brand>
-      <Nav className="mx-auto">
-        <Nav.Link href="/" className="px-3 nav-link-custom">
-          Analytics Dashboard
-        </Nav.Link>
-        {currentUser && (
-          <>
-            <Nav.Link href="/activity-log" className="px-3 nav-link-custom">
-              Activity Log
-            </Nav.Link>
-            <Nav.Link href="/user-management" className="px-3 nav-link-custom">
-              User Management
-            </Nav.Link>
-            <Nav.Link href="/kiosk-management" className="px-3 nav-link-custom">
-              Kiosk Management
-            </Nav.Link>
-          </>
-        )}
-      </Nav>
-      <Nav className="">
-        <Nav.Link href="/profile" className="nav-link-custom">
+    <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
+      <Container fluid>
+        <Navbar.Brand href="/" className="ms-3 nav-link-custom">
           <img
-            src="/imgs/profile.png"
+            src="/imgs/logo.png"
             alt="Profile"
             className="img-fluid"
-            style={{ width: "30px", height: "30px", borderRadius: "50%" }}
+            style={{
+              width: "30px",
+              height: "30px",
+              borderRadius: "50%",
+              marginRight: "10px",
+            }}
           />
-        </Nav.Link>
-      </Nav>
+          SCAN
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="/" className="px-3 nav-link-custom">
+              Analytics Dashboard
+            </Nav.Link>
+            {currentUser && (
+              <>
+                <Nav.Link href="/activity-log" className="px-3 nav-link-custom">
+                  Activity Log
+                </Nav.Link>
+                <Nav.Link
+                  href="/user-management"
+                  className="px-3 nav-link-custom"
+                >
+                  User Management
+                </Nav.Link>
+                <Nav.Link
+                  href="/kiosk-management"
+                  className="px-3 nav-link-custom"
+                >
+                  Kiosk Management
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+          <Nav>
+            <Nav.Link href="/profile" className="nav-link-custom">
+              <img
+                src="/imgs/profile.png"
+                alt="Profile"
+                className="img-fluid"
+                style={{ width: "30px", height: "30px", borderRadius: "50%" }}
+              />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
